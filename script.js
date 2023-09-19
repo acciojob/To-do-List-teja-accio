2ec26c36e0dda526638688cbad682a66f5db6f6c
teja-accio <128350434+teja-accio@users.noreply.github.com>
2023-09-19 08:51:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const newTodoInput = document.getElementById('newTodoInput');
+  const addTodoBtn = document.getElementById('addTodoBtn');
+  const todoList = document.getElementById('todoList');
+
+  addTodoBtn.addEventListener('click', () => {
+    const todoText = newTodoInput.value.trim();
+
+    if (todoText !== '') {
+      const todoItem = document.createElement('li');
+      todoItem.textContent = todoText;
+      todoList.appendChild(todoItem);
+      newTodoInput.value = ''; // Clear input after adding todo
+    }
+  });
+});
 
